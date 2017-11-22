b7e97521a187ffc1d6b5ce35e5dc49ce159acd23
AltVox <s177656@ucs.ac.uk>
2017-11-22 21:44:10 +0000

    Created the foundations of the colour palette.
    Created the foundations of the colour palette, that player can chose from in game.

---
+++ Game/GroupProjectGame/Assets/Scripts/ChangeColour2D.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColour2D : MonoBehaviour {
+
+    private static ChangeColour2D Instance; 
+    private Color colourSelected; 
+    private Color SelectedColour { get { return colourSelected; } } 
+
+   
+
+    void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this; 
+        }else if( Instance != this)
+        {
+            Destroy(this.gameObject); 
+        }
+
+        DontDestroyOnLoad(this.gameObject); 
+      
+    }
+
+    void Start () {
+        colourSelected = Color.grey; 
+	}
+	
+	public void ChangeColourRed()
+    {
+        colourSelected = Color.red;
+        Debug.Log("You have selected red"); 
+    }
+
+    public void ChangeColourBlue()
+    {
+        colourSelected = Color.blue;
+        Debug.Log("You have selected blue");
+    }
+
+    public void ChangeColourGreen()
+    {
+        colourSelected = Color.green;
+        Debug.Log("You have selected Green");
+    }
+}
