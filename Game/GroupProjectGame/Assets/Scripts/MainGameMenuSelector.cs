5788c1317645c5f9015e0e58e1616cf6a7100ef3
Charlie Crewe <C.Crewe@uos.ac.uk>
2017-11-23 20:34:47 +0000

    Uploading UI Task from last week
    Uploading the ui task from last week, have updated it with callums work
    from this week

---
+++ Game/GroupProjectGame/Assets/Scripts/MainGameMenuSelector.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainGameMenuSelector : MonoBehaviour
+{
+    bool MenuActive = false;
+    public GameObject go;
+    public Transform Target;
+
+    void Update ()
+    {
+        if ((MenuActive) && (go.transform.position.x >= 105))
+        {
+            Debug.Log("Moving Left");
+            go.transform.Translate(Vector3.left * 0.8f);
+        }
+
+        if ((!MenuActive) && (go.transform.position.x <= 160))
+        {
+            Debug.Log("Moving Right");
+            go.transform.Translate(Vector3.right * 0.8f);
+        }
+    }
+
+	public void OnClick ()
+    {
+        if (!MenuActive)
+        {
+            MenuActive = true;
+        }
+        else if (MenuActive)
+        {
+            MenuActive = false;
+        }
+	}
+}
